859fef566078a425dc4e7e4a564ea432aa686d2f
Dane Powell <git@danepowell.com>
2024-11-12 14:58:28 -0800

    CLI-1383: Upgrade consolidation/self-update (no bc break)

--- bin/logstream.php
+++ bin/logstream.php
@@ -8,9 +8,9 @@ $pharPath = \Phar::running(true);
 if ($pharPath) {
     $autoloaderPath = "$pharPath/vendor/autoload.php";
 } else {
-    if (file_exists(dirname(__DIR__).'/vendor/autoload.php')) {
-        $autoloaderPath = dirname(__DIR__).'/vendor/autoload.php';
-    } elseif (file_exists(dirname(__DIR__).'/../../autoload.php')) {
+    if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
+        $autoloaderPath = dirname(__DIR__) . '/vendor/autoload.php';
+    } elseif (file_exists(dirname(__DIR__) . '/../../autoload.php')) {
         $autoloaderPath = dirname(__DIR__) . '/../../autoload.php';
     } else {
         die("Could not find autoloader. Run 'composer install'.");
@@ -22,7 +22,12 @@ $version = trim(file_get_contents(dirname(__DIR__) . '/VERSION'));
 $application = new Application('Logstream', $version);
 $application->add(new LogstreamCommand());
 
-$selfUpdate = new SelfUpdateCommand('Logstream', $version, 'typhonius/acquia-logstream');
+if (class_exists('\SelfUpdate\SelfUpdateManager')) {
+    $selfUpdateManager = new \SelfUpdate\SelfUpdateManager('Logstream', $version, 'typhonius/acquia-logstream');
+    $selfUpdate = new SelfUpdateCommand($selfUpdateManager);
+} else {
+    $selfUpdate = new SelfUpdateCommand('Logstream', $version, 'typhonius/acquia-logstream');
+}
 $application->add($selfUpdate);
 
 $application->run();
